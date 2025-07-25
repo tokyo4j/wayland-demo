7e84a77f9fe8a6704616f13b03cba70cd0a69acd
tokyo4j <hrak1529@gmail.com>
2025-07-25 10:41:09 +0900

    support xdg_decoration

--- client.c
+++ client.c
@@ -1,3 +1,4 @@
+#include "xdg-decoration-protocol.h"
 #include "xdg-shell-protocol.h"
 #include <errno.h>
 #include <fcntl.h>
@@ -68,12 +69,14 @@ struct client_state {
 	struct xdg_wm_base *xdg_wm_base;
 	struct wl_seat *wl_seat;
 	struct wl_subcompositor *wl_subcompositor;
+	struct zxdg_decoration_manager_v1 *zxdg_decoration_manager_v1;
 	/* Objects */
 	struct wl_surface *wl_surface;
 	struct xdg_surface *xdg_surface;
 	struct xdg_toplevel *xdg_toplevel;
 	struct wl_pointer *wl_pointer;
 	struct wl_keyboard *wl_keyboard;
+	struct zxdg_toplevel_decoration_v1 *zxdg_toplevel_decoration_v1;
 
 	int width, height;
 
@@ -186,6 +189,11 @@ handle_registry_global(void *data, struct wl_registry *wl_registry,
 	} else if (!strcmp(interface, wl_seat_interface.name)) {
 		state->wl_seat = wl_registry_bind(
 			wl_registry, name, &wl_seat_interface, 8);
+	} else if (!strcmp(interface,
+			   zxdg_decoration_manager_v1_interface.name)) {
+		state->zxdg_decoration_manager_v1 =
+			wl_registry_bind(wl_registry, name,
+				&zxdg_decoration_manager_v1_interface, 1);
 	}
 }
 
@@ -282,6 +290,11 @@ main(int argc, char *argv[])
 	xdg_toplevel_add_listener(
 		state.xdg_toplevel, &xdg_toplevel_listener, &state);
 	xdg_toplevel_set_title(state.xdg_toplevel, "Example client");
+	state.zxdg_toplevel_decoration_v1 =
+		zxdg_decoration_manager_v1_get_toplevel_decoration(
+			state.zxdg_decoration_manager_v1, state.xdg_toplevel);
+	zxdg_toplevel_decoration_v1_set_mode(state.zxdg_toplevel_decoration_v1,
+		ZXDG_TOPLEVEL_DECORATION_V1_MODE_SERVER_SIDE);
 	wl_surface_commit(state.wl_surface);
 	wl_display_flush(state.wl_display);
 
