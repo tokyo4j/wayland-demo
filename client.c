2e8a132d6f949d699b9c88c071ca90497323d357
tokyo4j <hrak1529@gmail.com>
2026-02-16 16:30:03 +0900

    Add popup

--- client.c
+++ client.c
@@ -74,6 +74,18 @@ struct client_state {
 	struct xdg_toplevel *xdg_toplevel;
 	struct wl_pointer *wl_pointer;
 	struct wl_keyboard *wl_keyboard;
+	struct {
+		struct wl_surface *wl_surface;
+		struct xdg_surface *xdg_surface;
+		struct xdg_popup *xdg_popup;
+		struct xdg_positioner *xdg_positioner;
+		int width, height;
+
+		uint32_t popup_colors[2];
+		int popup_x;
+		int popup_anchor_width;
+		int popup_width;
+	} popup;
 
 	int width, height;
 
@@ -83,6 +95,14 @@ struct client_state {
 	struct {
 		int width, height;
 		uint32_t colors[2];
+
+		uint32_t popup_colors[2];
+		int popup_x;
+		int popup_y;
+		int popup_anchor_width;
+		int popup_anchor_height;
+		int popup_width;
+		int popup_height;
 	} defaults;
 };
 
@@ -215,6 +235,49 @@ on_wayland_event(uv_poll_t *handle, int status, int events)
 }
 
 static void
+handle_popup_xdg_surface_configure(
+	void *data, struct xdg_surface *xdg_surface, uint32_t serial)
+{
+	struct client_state *state = data;
+	xdg_surface_ack_configure(xdg_surface, serial);
+
+	struct wl_buffer *buffer = draw_frame(state->wl_shm, state->popup.width,
+		state->popup.height, state->defaults.popup_colors);
+	wl_surface_attach(state->popup.wl_surface, buffer, 0, 0);
+	wl_surface_commit(state->popup.wl_surface);
+}
+
+static const struct xdg_surface_listener popup_xdg_surface_listener = {
+	.configure = handle_popup_xdg_surface_configure,
+};
+
+static void
+handle_popup_xdg_popup_configure(void *data, struct xdg_popup *xdg_popup,
+	int32_t x, int32_t y, int32_t width, int32_t height)
+{
+	struct client_state *state = data;
+	state->popup.width = width;
+	state->popup.height = height;
+}
+
+static void
+handle_popup_xdg_popup_popup_done(void *data, struct xdg_popup *xdg_popup)
+{
+}
+
+static void
+handle_popup_xdg_popup_repositioned(
+	void *data, struct xdg_popup *xdg_popup, uint32_t token)
+{
+}
+
+static const struct xdg_popup_listener popup_xdg_popup_listener = {
+	.configure = handle_popup_xdg_popup_configure,
+	.popup_done = handle_popup_xdg_popup_popup_done,
+	.repositioned = handle_popup_xdg_popup_repositioned,
+};
+
+static void
 handle_xdg_toplevel_configure(void *data, struct xdg_toplevel *xdg_toplevel,
 	int32_t width, int32_t height, struct wl_array *states)
 {
@@ -226,6 +289,31 @@ handle_xdg_toplevel_configure(void *data, struct xdg_toplevel *xdg_toplevel,
 		state->width = state->defaults.width;
 		state->height = state->defaults.height;
 	}
+
+	/* create popup */
+	state->popup.wl_surface =
+		wl_compositor_create_surface(state->wl_compositor);
+	state->popup.xdg_surface = xdg_wm_base_get_xdg_surface(
+		state->xdg_wm_base, state->popup.wl_surface);
+	xdg_surface_add_listener(
+		state->popup.xdg_surface, &popup_xdg_surface_listener, state);
+	state->popup.xdg_positioner =
+		xdg_wm_base_create_positioner(state->xdg_wm_base);
+	xdg_positioner_set_anchor_rect(state->popup.xdg_positioner,
+		state->defaults.popup_x, state->defaults.popup_y,
+		state->defaults.popup_anchor_width,
+		state->defaults.popup_anchor_height);
+	xdg_positioner_set_anchor(
+		state->popup.xdg_positioner, XDG_POSITIONER_ANCHOR_TOP_LEFT);
+	xdg_positioner_set_gravity(
+		state->popup.xdg_positioner, XDG_POSITIONER_GRAVITY_BOTTOM_RIGHT);
+	xdg_positioner_set_size(state->popup.xdg_positioner,
+		state->defaults.popup_width, state->defaults.popup_height);
+	state->popup.xdg_popup = xdg_surface_get_popup(state->popup.xdg_surface,
+		state->xdg_surface, state->popup.xdg_positioner);
+	xdg_popup_add_listener(
+		state->popup.xdg_popup, &popup_xdg_popup_listener, state);
+	wl_surface_commit(state->popup.wl_surface);
 }
 
 static void
@@ -262,6 +350,14 @@ main(int argc, char *argv[])
 				.width = 600,
 				.height = 600,
 				.colors = {0xff666666, 0xffeeeeee},
+
+				.popup_colors = {0xffff6666, 0xffeeeeee},
+				.popup_x = 100,
+				.popup_y = 100,
+				.popup_width = 100,
+				.popup_height = 50,
+				.popup_anchor_width = 50,
+				.popup_anchor_height = 50,
 			},
 	};
 	state.wl_display = wl_display_connect(NULL);
